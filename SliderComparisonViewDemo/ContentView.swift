031c7580b301f6af4d22ffb3b761494ba09acbe2
Paresh Karnawat <paresh.karnawat@gmail.com>
2025-10-28 17:18:25 +0530

    Initial Commit

---
+++ SliderComparisonViewDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SliderComparisonViewDemo
+//
+//  Created by Paresh  Karnawat on 28/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SliderComparisonViewDemo/SliderComparisonViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderComparisonViewDemoApp.swift
+//  SliderComparisonViewDemo
+//
+//  Created by Paresh  Karnawat on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SliderComparisonViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
